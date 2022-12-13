3d0ef1f2150906bc89dd6afe3ddd1e1d6cd5645f
cskings14 <cskings14@gmail.com>
2022-12-13 03:48:06 -0500

    added authentication to the client side

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,21 +1,47 @@
+// import React from "react";
+// import ArticleList from "./components/article/ArticleListView";
+// import ArticleDetail from "./components/article/ArticleDetailView";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import React from "react";
-import ArticleList from "./components/article/ArticleListView";
-import ArticleDetail from "./components/article/ArticleDetailView";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Home from "./views/homePage";
+import Login from "./views/loginPage";
+import Register from "./views/registerPage";
+import ProtectedPage from "./views/ProtectedPage";
+
 
 function App() {
 
 
 
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<ArticleList />} />
-        <Route exact path="/:articleID" element={<ArticleDetail />} />
-      </Routes>
-      </BrowserRouter>
-    </div>
+    // <div className="App">
+    //   <BrowserRouter>
+    //   <Routes>
+    //     <Route exact path="/" element={<ArticleList />} />
+    //     <Route exact path="/:articleID" element={<ArticleDetail />} />
+    //   </Routes>
+    //   </BrowserRouter>
+    // </div>
+    <Router>
+      <div className="app">
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            {/* <PrivateRoute element={ProtectedPage} path="/protected" exact /> */}
+            <Route element={<Login />} path="/login" />
+            <Route element={<Register />} path="/register" />
+            <Route element={<Home />} path="/" />
+          </Routes>
+        </AuthProvider>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
