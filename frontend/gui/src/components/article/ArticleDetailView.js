930c2fcbe414f0adb583871608adf97d81d91def
cskings14 <cskings14@gmail.com>
2022-12-14 00:17:16 -0500

    fixed edit comment

--- frontend/gui/src/components/article/ArticleDetailView.js
+++ frontend/gui/src/components/article/ArticleDetailView.js
@@ -85,7 +85,7 @@ const ArticleDetail = () => {
         const content = e.target.content.value;
         const article = articleID;
 
-        const response = fetch(`http://127.0.0.1:8000/api/articles/${articleID}/comments/create/`, {
+        const response = fetch(`http://127.0.0.1:8000/api/comments/`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -119,26 +119,68 @@ const ArticleDetail = () => {
             alert("Something went wrong!");
         }
     }
-// if comment.article === article.id then make the comment
+
+    const handleEditCommentSubmit = e => {
+        e.preventDefault();
+        const author = user.username;
+        const commentID = e.target.id;
+        const content = e.target.content.value;
+        const article = articleID;
+        console.log(articleID);
+        
+        
+        // console.log(`http://127.0.0.1:8000/api/articles/${articleID}/comments/${commentID}/delete/`);
+        const response = fetch(`http://127.0.0.1:8000/api/comments/${commentID}/`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                content,
+                author,
+                article
+
+            })
+
+        });
+        if (response.status !== 400) {
+            navigate("/");
+        } else {
+            alert("Something went wrong!");
+        }
+    }
+    // if comment.article === article.id then make the comment
     function Component() {
         return data.map((comment) => (
             comment.article === article.id && (
-            <>
-                <Comment dataset={comment} />
-                {
-                    comment.author === user.username &&
-                    (
-                        <>
-                            <Form onSubmit={handleDeleteCommentSubmit} className="form" id={comment.id}>
-                                <Button variant="primary" type="submit">
-                                    Submit Deletion
-                                </Button>
-                            </Form>
-                        </>
-                    )
-                }
-            </>
-        ))
+                <>
+                    <Comment dataset={comment}/>
+                    {
+                        comment.author === user.username &&
+                        (
+                            <>
+                                <Form onSubmit={handleEditCommentSubmit} className="form" id={comment.id}>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Content</Form.Label>
+                                        <Form.Control type="content" placeholder="Edit Content" id="content" />
+                                    </Form.Group>
+
+                                    <Button variant="primary" type="submit">
+                                        Submit Edit
+                                    </Button>
+                                </Form>
+
+
+                                <Form onSubmit={handleDeleteCommentSubmit} className="form" id={comment.id}>
+                                    <Button variant="primary" type="submit">
+                                        Submit Deletion
+                                    </Button>
+                                </Form>
+                            </>
+                        )
+                    }
+                </>
+            ))
         )
     }
 
