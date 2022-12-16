d6421b6a82e20102d6d103d9cab9675f28747d06
cskings14 <cskings14@gmail.com>
2022-12-16 08:13:00 -0500

    changed settings

--- onlyspeak/settings.py
+++ onlyspeak/settings.py
@@ -152,9 +152,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ],
 }
 # needed to rest framework authentication
 
